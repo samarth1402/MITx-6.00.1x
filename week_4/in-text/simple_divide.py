81d644533403bd3114395a765980ecb4970656f8
Samarth Singhal <samarthsinghal1402@gmail.com>
2018-01-20 16:50:51 +0530

    in-text question for week 4 added

---
+++ week_4/in-text/simple_divide.py
@@ -0,0 +1,23 @@
+# Exercise: simple divide
+
+# Suppose we rewrite the FancyDivide function to use a helper function.
+
+# def fancy_divide(list_of_numbers, index):
+#    denom = list_of_numbers[index]
+#    return [simple_divide(item, denom) for item in list_of_numbers]
+
+# def simple_divide(item, denom):
+#    return item / denom
+
+# This code raises a ZeroDivisionError exception for the following call: fancy_divide([0, 2, 4], 0)
+
+# Your task is to change the definition of simple_divide so that the call does not raise an exception.
+# When dividing by 0, fancy_divide should return a list with all 0 elements.
+# Any other error cases should still raise exceptions. You should only handle the ZeroDivisionError.
+
+
+def simple_divide(item, denom):
+      try:
+          return item / denom
+      except ZeroDivisionError:
+          return 0
