fefccc55021f7d021a663b85c6eaaac456dfe417
Samarth Singhal <samarthsinghal1402@gmail.com>
2018-01-26 21:57:12 +0530

    animals example from video with inheritence

--- week_5/in-text/video_ques/animal.py
+++ week_5/in-text/video_ques/animal.py
@@ -18,4 +18,53 @@ class Animal (object):
     def __str__(self):
         return "animal: "+str(self.name)+" : "+str(self.age)
 
-    
+class Cat(Animal):
+    def speak(self):
+        print("meow")
+    def __str__(self):
+        return "cat: "+str(self.name)+" : "+str(self.age)
+
+"""
+jelly = Cat(1)
+jelly.set_name("JellyBelly")
+print(jelly)
+
+print(Animal.__str__(jelly))
+
+bob = Animal(1)
+print(bob)
+
+bob.set_name()
+print(bob)
+"""
+
+class Rabbit(Animal):
+    def speak(self):
+        print("meep")
+    def __str__(self):
+        return "rabbit: "+str(self.name)+" : "+str(self.age)
+
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self,age)
+        Animal.set_name(self, name)
+        self.friends = []
+
+    def get_friends(self):
+        return self.friends
+
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+
+    def speak(self):
+        print("hello")
+
+    def age_diff(self, other):
+        diff = self.get_age()-other.get_age()
+        if self.age > other.age:
+            print(self.name , "is", diff, "years older than", other.name)
+        else:
+            print(self.name, "is", -diff, "years younger than", other.name)
+    def __str__(self):
+        return "person: "+str(self.name)+" : "+str(self.age)
