fe50e63c615eddfcf5906fc14b0d41a661bf8d07
Samarth Singhal <samarthsinghal1402@gmail.com>
2018-01-26 22:46:56 +0530

    class variable concept for video ques on animal added

--- week_5/in-text/video_ques/animal.py
+++ week_5/in-text/video_ques/animal.py
@@ -18,6 +18,7 @@ class Animal (object):
     def __str__(self):
         return "animal: "+str(self.name)+" : "+str(self.age)
 
+
 class Cat(Animal):
     def speak(self):
         print("meow")
@@ -38,11 +39,6 @@ bob.set_name()
 print(bob)
 """
 
-class Rabbit(Animal):
-    def speak(self):
-        print("meep")
-    def __str__(self):
-        return "rabbit: "+str(self.name)+" : "+str(self.age)
 
 class Person(Animal):
     def __init__(self, name, age):
@@ -68,3 +64,38 @@ class Person(Animal):
             print(self.name, "is", -diff, "years younger than", other.name)
     def __str__(self):
         return "person: "+str(self.name)+" : "+str(self.age)
+
+
+class Rabbit(Animal):
+    tag = 1
+    def __init__(self, age, parent1=None, parent2=None):
+        Animal.__init__(self,age)
+        self.parent1 = parent1
+        self.parent2 = parent2
+        self.rid = Rabbit.tag
+        Rabbit.tag += 1
+    def get_rid(self):
+        return str(self.rid).zfill(3)
+    def get_parent1(self):
+        return self.parent1
+    def get_parent(self):
+        return self.parent2
+    def __add__(self, other):
+        #r1 and r2 are 2 rabbits, r3 = r1+r2 will give a new rabbit r3
+        return Rabbit(0,self,other)
+    def __eq__(self, other):
+        parents_same = self.parent1.rid == other.parent1.rid and self.parent2.rid == other.parent2.rid
+        parents_opposite = self.parent1.rid == other.parent1.rid and self.parent1.rid == other.parent2.rid
+        return parents_same or parents_opposite
+
+"""
+peter = Rabbit(2)
+peter.set_name('Peter')
+hopsy = Rabbit(3)
+hopsy.set_name('Hopsy')
+cotton = Rabbit(1,peter,hopsy)
+cotton.set_name('cottontail')
+mopsy = peter + hopsy
+mopsy.set_name('Mopsy')
+print(mopsy == cotton)
+"""
