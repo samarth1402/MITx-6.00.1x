10eaba17de7c87918089401ee22539c92483d7e0
Samarth Singhal <samarthsinghal1402@gmail.com>
2018-01-26 19:31:16 +0530

    video ques for week 5 added

---
+++ week_5/in-text/video_ques/animal.py
@@ -0,0 +1,21 @@
+class Animal (object):
+    def __init__(self,age):
+        self.age = age
+        self.name = None
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+
+    def set_age(self,newage):
+        self.age = newage
+
+    def set_name(self,newname=""):
+        self.name = newname
+
+    def __str__(self):
+        return "animal: "+str(self.name)+" : "+str(self.age)
+
+    
