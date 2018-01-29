57064df44d417f8abe8b68b3a899fe1c59997efd
Samarth Singhal <samarthsinghal1402@gmail.com>
2018-01-29 20:04:58 +0530

    Added gradebook example and usage of generator

--- week_5/in-text/video_ques/information_system.py
+++ week_5/in-text/video_ques/information_system.py
@@ -138,3 +138,63 @@ class Professor(MITPerson):
     def lecture(self, topic):
         return self.speak('it is obvious that ' + topic)
 
+#
+#Gradebook example
+#
+class Grades(object):
+    """A mapping from students to a list of grades"""
+
+    def __init__(self):
+        """Create empty grade book"""
+        self.students = []   #list of Studnet objects
+        self.grades = []    # maps idNum -> list of grades
+        self.isSorted = True    # true if self.students is sorted
+
+    def addStudnet(self, student):
+        """ Assumes: student is of type Student
+            Add student to the grade book"""
+        if student in self.students:
+            raise ValueError('Duplicate student')
+        self.students.append(student)
+        self.grades[student.getIdNum()] = {}
+        self.isSorted = False
+
+    def addGrade(self, student, grade):
+        """Assumes: grade is a float.
+           Add grade to the list of grades for student"""
+        try:
+            self.grades[student.getIdNum()].append(grade)
+        except KeyError:
+            raise ValueError('Student not in grade book')
+
+    def getGrades(self, student):
+        """Return a list of grades for student"""
+        try:    #return copy of student's grades
+            return self.grades[student.getIdNum()][:]
+        except KeyError:
+            raise ValueError('Student not in grade book')
+
+    def allStudents(self):
+        """Return a list of students in the grade book"""
+        if not self.isSorted:
+            self.students.sort()
+            self.isSorted = True
+        for s in self.students:
+            yield s
+
+
+def gradeReport(course):
+    """Assumes: course if of type grades"""
+    report = []
+    for s in course.allStudents()
+        tot = 0.0
+        numGrades = 0
+        for g in course.getGrades(s):
+            tot += g
+            numGrades += 1
+        try:
+            average = tot/numGrades
+            report.append(str(s) + ' \'s mean grade is ' + str(average) )
+        except ZeroDivisionError:
+            report.append(str(s) + ' has no grades')
+    return '\n'.join(report)
