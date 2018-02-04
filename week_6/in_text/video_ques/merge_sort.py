d4d4ebea84e87681e2ea3810cd565aeb93250779
Samarth Singhal <samarthsinghal1402@gmail.com>
2018-02-04 15:23:13 +0530

    Algo for merge sort added

---
+++ week_6/in_text/video_ques/merge_sort.py
@@ -0,0 +1,26 @@
+def merge (left, right):
+    result = []
+    i,j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while (i < len(left)):
+        result.append(left[i])
+        i += 1
+    while (j < len(right)):
+        result.append(right[j])
+        j +=  1
+    return result
+
+def merge_sort(L):
+    if len(L) <= 1:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
